fbdc76644bd06d4e09c3433052e19b5e513641fa
baiysh1218 <baiyshbeksultanov1@gmail.com>
2022-11-08 15:16:01 +0600

    added random

--- src/components/quesion/quesionList/QuesionList.jsx
+++ src/components/quesion/quesionList/QuesionList.jsx
@@ -18,12 +18,11 @@ const QuesionList = () => {
   }, []);
 
   let random = arrayRandElement(question);
-  let answer = random.answer;
 
   return (
     <div className="que-list">
       <h1 className="que-h1">{random.question}</h1>
-      <h3 className="que-h3">длинна ответа {answer.length}</h3>
+      {/* <h3 className="que-h3">длинна ответа {answer.length}</h3> */}
       <QuestionRandom props={random} />
     </div>
   );
--- src/components/quesion/quesionList/QuestionRandom.jsx
+++ src/components/quesion/quesionList/QuestionRandom.jsx
@@ -7,41 +7,51 @@ import "./Question.css";
 const QuestionRandom = ({ props }) => {
   const [answerToQuestion, setAnswerToQuestion] = useState("");
   const [one, setOne] = useState("");
+  const [answerArray, setAnswerArray] = useState([]);
+  // console.log(answerArray);
 
   let answer = props.answer;
 
-  function handleSave1() {
-    for (let i = 0; i < answer.length; i++) {
-      for (let j = 0; j < answerToQuestion.length; j++) {
-        if (answer[i].toUpperCase() === answerToQuestion[j].toUpperCase()) {
-          var res = "Ответ правильный!";
-        }
-      }
-    }
-    alert(res);
-  }
+  // function handleSave1() {
+  //   for (let i = 0; i < answer.length; i++) {
+  //     for (let j = 0; j < answerToQuestion.length; j++) {
+  //       if (answer[i].toUpperCase() === answerToQuestion[j].toUpperCase()) {
+  //         var res = "Ответ правильный!";
+  //       }
+  //     }
+  //   }
+  //   alert(res);
+  // }
 
   function handleSave2() {
-    if (one > 1) {
-      alert("вводите только одну букву");
-    }
     for (let i = 0; i < answer.length; i++) {
-      if (answer[i] === one) {
-        var res = "ответ правильный!";
-      } else if (answer[i] !== one) {
-        var res2 = "Не правильная буква";
+      answerArray[i] = "_";
+    }
+
+    answerArray.map((item, index) => {
+      if (answer[index] === one) {
+        return (answerArray[index] = one);
+      } else {
+        return item;
       }
-    }
-    if (res) {
-      alert(res);
-    } else {
-      alert(res2);
-    }
+    });
+
+    setOne("");
   }
+  console.log(answerArray);
 
   return (
     <div>
       <div className="modal-question">
+        <h2>{answerArray}</h2>
+        <h2>{answer.length}</h2>
+
+        {answer.toUpperCase() === answerToQuestion.toUpperCase() ? (
+          <div>правильное слово</div>
+        ) : (
+          <div>Ждём праивльного ответа!</div>
+        )}
+
         <TextField
           id="outlined-search"
           label="Введите букву"
@@ -51,18 +61,29 @@ const QuestionRandom = ({ props }) => {
           }}
           value={one}
           onChange={e => setOne(e.target.value)}
-          placeholder="Введите слово"
         />
-
-        <Button
-          onClick={() => handleSave2()}
-          variant="contained"
-          color="success"
-          style={{
-            marginTop: "15px",
-          }}>
-          проверить букву
-        </Button>
+        {one.length === 0 ? (
+          <Button
+            // onClick={() => handleSave2()}
+            variant="contained"
+            disabled
+            color="error"
+            style={{
+              marginTop: "15px",
+            }}>
+            Введите букву
+          </Button>
+        ) : (
+          <Button
+            onClick={() => handleSave2()}
+            variant="contained"
+            color="success"
+            style={{
+              marginTop: "15px",
+            }}>
+            проверить букву
+          </Button>
+        )}
       </div>
       <div className="modal-question">
         <TextField
@@ -75,15 +96,28 @@ const QuestionRandom = ({ props }) => {
           value={answerToQuestion}
           onChange={e => setAnswerToQuestion(e.target.value)}
         />
-        <Button
-          onClick={() => handleSave1()}
-          variant="contained"
-          color="success"
-          style={{
-            marginTop: "15px",
-          }}>
-          проверить слово
-        </Button>
+        {answerToQuestion.length === 0 ? (
+          <Button
+            // onClick={() => handleSave1()}
+            variant="contained"
+            disabled
+            color="error"
+            style={{
+              marginTop: "15px",
+            }}>
+            Введите слово
+          </Button>
+        ) : (
+          <Button
+            // onClick={() => handleSave1()}
+            variant="contained"
+            color="success"
+            style={{
+              marginTop: "15px",
+            }}>
+            проверить слово
+          </Button>
+        )}
       </div>
     </div>
   );
